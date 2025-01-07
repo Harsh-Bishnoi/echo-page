5bfde6ea98d85c42803d17979466cb675d81493f
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-01-07 15:13:36 +0530

    change in all section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,18 @@
+// navbar 
+const navbarbtn = () => {
+    document.getElementById("name").classList.toggle("show-navbar")
+    document.getElementById("rotates").classList.toggle("rotate40")
+    document.getElementById("transparent").classList.toggle("bg-transparent")
+    document.getElementById("rotates2").classList.toggle("rotate-45")
+    document.body.classList.toggle("overflow-hidden")
+}
+
+// slider 
 $('.slider').slick({
-    infinite: true,
-    dots: true,
-    arrows: false,
     slidesToShow: 3,
     slidesToScroll: 1,
+    arrows: false,
+    dots: false,
     responsive: [
         {
             breakpoint: 1024,
@@ -15,20 +24,14 @@ $('.slider').slick({
             }
         },
         {
-            breakpoint: 600,
+            breakpoint: 768,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                dots: true,
             }
         },
+
+
     ]
 });
-
-
-const navbarbtn = () => {
-    document.getElementById("name").classList.toggle("show-navbar")
-    document.getElementById("rotates").classList.toggle("rotate40")
-    document.getElementById("transparent").classList.toggle("bg-transparent")
-    document.getElementById("rotates2").classList.toggle("rotate-45")
-    document.body.classList.toggle("overflow-hidden")
-}
